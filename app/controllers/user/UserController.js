adca12f7825c30b8fcaa2494becc60e4e1fbd7a8
Gom3a98 <kkrem551@gmail.com>
2019-12-08 18:40:36 +0200

    user design and upload cv front only

--- app/controllers/user/UserController.js
+++ app/controllers/user/UserController.js
@@ -59,6 +59,46 @@ module.exports={
             res.redirect('/login')
         //Authentication(req,res).then(res.send('Welcome back, ' + req.session.userName + '!'));
 
-        }
+        },
+	uploadCV : function(req, res){
+		message = '';
+		console.log(req);
+		/*
+		if(req.method == "POST"){
+			var post  = req.body;
+			var name= post.user_name;
+			var pass= post.password;
+			var fname= post.first_name;
+			var lname= post.last_name;
+			var mob= post.mob_no;
+
+			if (!req.files)
+				return res.status(400).send('No files were uploaded.');
+
+			var file = req.files.uploaded_image;
+			var img_name=file.name;
+
+			if(file.mimetype == "image/jpeg" ||file.mimetype == "image/png"||file.mimetype == "image/gif" ){
+
+				file.mv('public/images/upload_images/'+file.name, function(err) {
+
+					if (err)
+
+						return res.status(500).send(err);
+					var sql = "INSERT INTO `users_image`(`first_name`,`last_name`,`mob_no`,`user_name`, `password` ,`image`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "','" + img_name + "')";
+
+					var query = db.query(sql, function(err, result) {
+						res.redirect('profile/'+result.insertId);
+					});
+				});
+			} else {
+				message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
+				res.render('index.ejs',{message: message});
+			}
+		} else {
+			res.render('index');
+		}
+*/
+	}
 };
 
--- public/gulpfile.js
+++ public/gulpfile.js
@@ -35,8 +35,8 @@ function modules() {
     .pipe(gulp.dest('./vendor/bootstrap'));
   // jQuery
   var jquery = gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
+      '../node_modules/jquery/dist/*',
+      '!../node_modules/jquery/dist/core.js'
     ])
     .pipe(gulp.dest('./vendor/jquery'));
   return merge(bootstrap, jquery);
